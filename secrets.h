26548c2f2f241795c2e0e37a68b987f59bd1cb4d
venu <adusumallivenugopal04@gmail.com>
2025-03-15 05:06:32 +0530

    Added secrets.h to .gitignore

--- secrets.h
+++
@@ -1,8 +0,0 @@
-#ifndef SECRETS_H
-#define SECRETS_H
-
-#define SSID "Nxtwave_Tech"
-#define PASSWORD "NWTech@2025"
-#define GROQ_API_KEY "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"
-
-#endif
